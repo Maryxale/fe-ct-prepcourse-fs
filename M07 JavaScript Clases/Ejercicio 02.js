4f82709dd9f09c1dc7299baa15b6c18449db33b2
Maryxale <romerobencomo@gmail.com>
2023-08-29 14:13:27 -0300

    my homework modulo 07 lista

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,21 +6,51 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre,apellido,edad,domicilio){
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+   detalle(){
+      return {nombre: this.nombre, apellido: this.apellido, edad: this.edad, domicilio: this.domicilio};
+      
+   }
 }
+var primeraPersona = new Persona('Luisa','Bencomo',65,'French 92');
+primeraPersona.detalle();
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   var Persona2 = new Persona(nombre,apellido,edad,domicilio);
+   return Persona2;
+
+   
 }
+
+
+
+
 
 function agregarMetodo() {
    // La función agrega un método "datos" a la clase "Persona".
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   
+      Persona.prototype.datos = function(){
+         return `${this.nombre}, ${this.edad} años`;
+      }
+   
+   
 }
+agregarMetodo();
+var otraPersona = new Persona("juan",22);
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
